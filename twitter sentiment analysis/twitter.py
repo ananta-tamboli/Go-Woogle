d96c48bb7918ee32c8f53e277c43283845b42128
ananta-tamboli <78536047+ananta-tamboli@users.noreply.github.com>
2021-02-08 17:09:27 +0530

    Update twitter.py

--- twitter sentiment analysis/twitter.py
+++ twitter sentiment analysis/twitter.py
@@ -85,7 +85,7 @@ def main():
     # creating object of TwitterClient Class
     api = TwitterClient()
     # calling function to get tweets
-    tweets = api.get_tweets(query = 'Rahul Gandhi', count = 200)
+    tweets = api.get_tweets(query = 'Akshay Kumar', count = 200)
  
     # picking positive tweets from tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
